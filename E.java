6c4d5d290580d8cfce5da2e4c0b8d63b23d55a4d
FARIDHANISHA <108788056+FARIDHANISHA@users.noreply.github.com>
2022-07-21 07:16:37 -0700

    Add files via upload

---
+++ E.java
@@ -0,0 +1,18 @@
+class E{
+    public  static void main (String []args)
+    {
+        int i=10;
+        int j=10;
+        System.out.println(i+j);
+        System.out.println(i-j);
+System.out.println("Hello Java");
+System.out.println("1");
+System.out.println("987654321");
+System.out.println("false");
+System.out.println("0601110");
+System.out.println("Hello Java");
+
+
+
+    }
+}  
